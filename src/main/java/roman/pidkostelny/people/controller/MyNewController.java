f93f68bba331254e2497ce7102592435cbfbaf3e
Roman PIdkostelnyi <pidkostelnyi@gmail.com>
2019-03-18 17:50:40 +0200

    sd

---
+++ src/main/java/roman/pidkostelny/people/controller/MyNewController.java
@@ -0,0 +1,9 @@
+package roman.pidkostelny.people.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/some")
+public class MyNewController {
+}
